5368edcfee53a8e19c685d0ca7dfe3527535d98b
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-04-29 17:29:00 -0400

    set up angular routes

---
+++ public/js/appRoutes.js
@@ -0,0 +1,17 @@
+angular.module('appRoutes', []).configure(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $routeProvider
+    // homepage
+    .when('/', {
+      templateUrl: 'views/home.html',
+      controller: 'MainController'
+    })
+
+    // nerds page 
+    .when('/nerds', {
+      templateUrl: 'views/nerd.html',
+      controller: 'NerdController'
+    });
+
+  $locationProvider.html5Mode(true)
+
+}]);
